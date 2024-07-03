337c2bbdecd80592e578be688363eb55aabe0311
Gomyeunghan <rhaudgks04@gmail.com>
2024-07-03 11:14:57 +0900

    shadowDom

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
 class Button extends HTMLElement {
   constructor() {
     super();
+
+    this.attachShadow({ mode: 'open' }); // 생성된 컴포넌트를 가르킴
+    this.shadowRoot.innerHTML = `
+    <button>Hello</button>`;
   }
 
   connectedCallback() {}
@@ -8,4 +12,8 @@ class Button extends HTMLElement {
   disconnectedCallback() {}
 }
 
-customElements.define('c-button', Button, { extends: 'button' });
+customElements.define('c-button', Button);
+
+console.log(
+  document.querySelector('c-button').shadowRoot.querySelector('button')
+);
